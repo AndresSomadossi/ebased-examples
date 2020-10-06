8759d87e70b92f7d60d53f8156fde005cafd5b11
Andres Somadossi <andressomadossi@gmail.com>
2020-10-06 12:00:50 -0300

    fix: using mapping

--- src/account/createWithdrawal.js
+++ src/account/createWithdrawal.js
@@ -1,12 +1,9 @@
-const { batchIterator } = require('ebased/handler/input/batchEventQueue');
-const { processingFinished, processingFinishedError } = require('ebased/handler/output/eventConfirmation');
+const { batchEventMapper } = require('ebased/handler');
+const inputMode = require('ebased/handler/input/batchEventQueue');
+const outputMode = require('ebased/handler/output/eventConfirmation');
+
+const domain = async (eventPayload) => ({ body: eventPayload });
 
 module.exports.handler = async (events, context) => {
-  try {
-    console.log(JSON.stringify(events));
-    const domain = ({ eventPayload }) => ({ body: eventPayload });
-    await batchIterator(events, context, domain, processingFinished);
-  } catch (error) {
-    return processingFinishedError(error);
-  }
+  return batchEventMapper({ events, context }, inputMode, domain, outputMode);
 }
